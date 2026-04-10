d74a1e7710a2c03046653acd7570bb944468ade7
Emmatorres25 <emmatorres25@gmail.com>
2026-04-09 23:22:57 -0600

    App Repartidor AkyVoy - login, pedidos activos, historial, perfil

---
+++ conexion.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://vjjwnouuejlkcnnpfims.supabase.co';
+const supabaseKey = 'sb_publishable_GcsVuC4xIxXnL9BdePgd-g_F_XxUFjB';
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
